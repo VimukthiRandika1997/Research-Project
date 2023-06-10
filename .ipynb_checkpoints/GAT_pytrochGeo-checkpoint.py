20e6edd4ae6b296b3f7b6230a6a775358618cf24
VimukthiRandika1997 <vimukthirandika1997@gmail.com>
2023-06-11 00:54:13 +0530

    GAT with Min-batch training

---
+++ .ipynb_checkpoints/GAT_pytrochGeo-checkpoint.py
@@ -0,0 +1,65 @@
+from torch_geometric.datasets import Planetoid
+import torch
+import torch.nn.functional as F
+from torch_geometric.nn import GATv2Conv
+from torch.nn import Linear, Dropout
+
+# Loading the dataset
+dataset = Planetoid(root='./data', name='Cora')
+data = dataset[0]
+print(data)
+
+
+# Accuracy function
+def accuracy(y_pred, y_true):
+    return torch.sum(y_pred == y_true) / len(y_true)
+
+
+class GAT(torch.nn.Module):
+    def __init__(self, dim_in, dim_h, dim_out, heads=8):
+        super().__init__()
+        self.gat1 = GATv2Conv(dim_in, dim_h, heads=heads)
+        self.gat2 = GATv2Conv(dim_h * heads, dim_out, heads=1)
+
+    def forward(self, x, edge_index):
+        h = F.dropout(x, p=0.6, training=self.training)
+        h = self.gat1(h, edge_index)
+        h = F.elu(h)
+        h = F.dropout(h, p=0.6, training=self.training)
+        h = self.gat2(h, edge_index)
+        return F.log_softmax(h, dim=1)
+
+    def fit(self, data, epochs):
+        criterion = torch.nn.CrossEntropyLoss()
+        optimizer = torch.optim.Adam(self.parameters(), lr=0.01, weight_decay=0.01)
+
+        self.train()
+        for epoch in range(epochs + 1):
+            optimizer.zero_grad()
+            out = self(data.x, data.edge_index)
+            loss = criterion(out[data.train_mask], data.y[data.train_mask])
+            acc = accuracy(out[data.train_mask].argmax(dim=1), data.y[data.train_mask])
+            loss.backward()
+            optimizer.step()
+
+            if epoch % 20 == 0:
+                val_loss = criterion(out[data.val_mask], data.y[data.val_mask])
+                val_acc = accuracy(out[data.val_mask].argmax(dim=1), data.y[data.val_mask])
+                print(
+                    f'Epoch {epoch:>3} | Train Loss: {loss:.3f} | Train Acc: {acc * 100:>5.2f}% | Val Loss: {val_loss:.2f} | Val Acc: {val_acc * 100:.2f}%')
+
+    @torch.no_grad()
+    def test(self, data):
+        self.eval()
+        out = self(data.x, data.edge_index)
+        acc = accuracy(out.argmax(dim=1)[data.test_mask], data.y[data.test_mask])
+        return acc
+
+
+def run():
+    gat = GAT(dataset.num_features, 32, dataset.num_classes)
+    print(gat)
+    gat.fit(data, epochs=100)
+
+    acc = gat.test(data)
+    print(f'GAT test accuracy: {acc * 100:.2f}%')
