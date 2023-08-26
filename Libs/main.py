cbcc479c7f27ce139a7fe35541b8de48f7decb22
VimukthiRandika1997 <vimukthirandika1997@gmail.com>
2023-08-27 04:47:42 +0530

    Train is added

---
+++ Libs/main.py
@@ -0,0 +1,18 @@
+import os
+import time
+import argparse
+
+from tqdm import tqdm
+
+
+def arg_parser():
+    pass
+
+
+def run():
+    pass
+
+
+if __name__ == '__main__':
+    # arg_parser()
+    run()
--- Libs/train.py
+++ Libs/train.py
@@ -1,5 +1,8 @@
+################### - Imports - #############################################################
 import numpy as np
 import matplotlib.pyplot as plt
+import plotly.express as px
+import networkx as nx
 import pandas as pd
 
 import torch
@@ -11,42 +14,87 @@ import os
 import time
 from tqdm import tqdm
 
-from utils import create_early_stopper
+from Libs.utils import create_early_stopper, seed_everything
+
+import warnings
+warnings.filterwarnings("ignore")
+##################################
+############################################################################################
+
+
+################### - Paths - #############################################################
+base_path_for_saving_artifacts = './Runs'
+
+
+##################################
+############################################################################################
+
+
+################### - Modular Functions - #############################################################
+def _create_directories(experiment_name):
+    """Creating directories for saving artifacts for a specific experiment / run"""
+
+    # For the base path
+    if not os.path.exists(base_path_for_saving_artifacts):
+        os.makedirs(base_path_for_saving_artifacts, exist_ok=True)
+
+    path_to_saving = os.path.join(
+        base_path_for_saving_artifacts, str(experiment_name))
+    if not os.path.exists(path_to_saving):
+        # For saving images
+        os.makedirs(os.path.join(path_to_saving, 'images'), exist_ok=True)
+        # For saving model weights
+        os.makedirs(os.path.join(path_to_saving, 'models'), exist_ok=True)
+
+    return path_to_saving
+
 
 def build_optimizer(args, params):
+    """Build an optimizer and scheduler"""
+
     weight_decay = args.weight_decay
     filter_fn = filter(lambda param: param.requires_grad, params)
 
     if args.opt == 'adam':
-        optimizer = optim.Adam(filter_fn, lr=args.lr, weight_decay=weight_decay)
+        optimizer = optim.Adam(filter_fn, lr=args.lr,
+                               weight_decay=weight_decay)
     elif args.opt == 'sgd':
-        optimizer = optim.SGD(filter_fn, lr=args.lr, momentum=0.95, weight_decay=weight_decay)
+        optimizer = optim.SGD(filter_fn, lr=args.lr,
+                              momentum=0.95, weight_decay=weight_decay)
     elif args.opt == 'rmsprop':
-        optimizer = optim.RMSprop(filter_fn, lr=args.lr, weight_decay=weight_decay)
+        optimizer = optim.RMSprop(
+            filter_fn, lr=args.lr, weight_decay=weight_decay)
     elif args.opt == 'adagrad':
-        optimizer = optim.Adagrad(filter_fn, lr=args.lr, weight_decay=weight_decay)
+        optimizer = optim.Adagrad(
+            filter_fn, lr=args.lr, weight_decay=weight_decay)
     else:
-        optimizer = optim.Adam(filter_fn, lr=args.lr, weight_decay=weight_decay)
+        optimizer = optim.Adam(filter_fn, lr=args.lr,
+                               weight_decay=weight_decay)
 
     if args.opt_sheduler == 'none':
         return None, optimizer
     elif args.opt_sheduler == 'step':
-        scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=args.opt_decay_step, gamma=args.opt_decay_rate)
+        scheduler = optim.lr_scheduler.StepLR(
+            optimizer, step_size=args.opt_decay_step, gamma=args.opt_decay_rate)
     elif args.opt_scheduler == 'cos':
-        scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=args.opt_restart)
+        scheduler = optim.lr_scheduler.CosineAnnealingLR(
+            optimizer, T_max=args.opt_restart)
     else:
-        scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=args.opt_decay_step, gamma=args.opt_decay_rate)
+        scheduler = optim.lr_scheduler.StepLR(
+            optimizer, step_size=args.opt_decay_step, gamma=args.opt_decay_rate)
 
     return scheduler, optimizer
 
 
 def accuracy(pred_y, y):
+    """Accuracy of the model: supervised learning"""
     return ((pred_y == y).sum() / len(y)).item()
 
 
 def train(model, loader, val_loader, epochs=100, edge_feature_compact=True):
     """Train and validate the model with given params
 
+    :param val_loader:
     :param edge_feature_compact:
     :param model:
     :param loader:
@@ -76,7 +124,8 @@ def train(model, loader, val_loader, epochs=100, edge_feature_compact=True):
             optimizer.zero_grad()
             with torch.cuda.amp.autocast():  # Mix-precision with autocast
                 if edge_feature_compact:
-                    out = model(data.x, data.edge_index, data.edge_attr, data.batch)
+                    out = model(data.x, data.edge_index,
+                                data.edge_attr, data.batch)
                 else:
                     out = model(data.x, data.edge_index, data.batch)
                 loss = criterion(out, data.y)
@@ -87,7 +136,8 @@ def train(model, loader, val_loader, epochs=100, edge_feature_compact=True):
             scaler.update()
 
         # Validation for each epoch
-        val_loss, val_acc = validate_model(model, val_loader, edge_feature_compact)
+        val_loss, val_acc = validate_model(
+            model, val_loader, edge_feature_compact)
 
         val_loss_list.append(val_loss.detach().cpu().numpy().item())
         val_acc_list.append(val_acc)
@@ -100,6 +150,7 @@ def train(model, loader, val_loader, epochs=100, edge_feature_compact=True):
             print(
                 f'Epoch: {epoch:03d}, Train Acc: {acc * 100:.4f}, Val Acc: {val_acc * 100:.4f}, Train Loss: {total_loss:.2f}, Val Loss: {val_loss:.4f}')
 
+        # Early stopper
         if early_stopper.early_stop(val_loss):
             break
 
@@ -113,6 +164,7 @@ def train(model, loader, val_loader, epochs=100, edge_feature_compact=True):
 @torch.no_grad()
 def validate_model(model, loader, edge_feature_compact=True):
     """Evaluate the model on evaluation mode"""
+
     criterion = torch.nn.CrossEntropyLoss()
     model.eval()
     loss = 0
@@ -121,20 +173,22 @@ def validate_model(model, loader, edge_feature_compact=True):
     for data in loader:
         with torch.cuda.amp.autocast():  # Mix-precision
             if edge_feature_compact:
-                out = model(data.x, data.edge_index, data.edge_attr, data.batch)
+                out = model(data.x, data.edge_index,
+                            data.edge_attr, data.batch)
             else:
                 out = model(data.x, data.edge_index, data.batch)
 
             loss += criterion(out, data.y) / len(loader)
             acc += accuracy(out.argmax(dim=1), data.y) / len(loader)
 
-    model.train()
+    model.train()  # setting model back to train mode
     return loss, acc
 
 
 @torch.no_grad()
 def test_model(model, loader, edge_feature_compact=True):
     """Evaluate the model on evaluation mode"""
+
     criterion = torch.nn.CrossEntropyLoss()
     model.eval()
     loss = 0
@@ -143,18 +197,22 @@ def test_model(model, loader, edge_feature_compact=True):
     for data in loader:
         with torch.cuda.amp.autocast():  # Mix-precision
             if edge_feature_compact:
-                out = model(data.x, data.edge_index, data.edge_attr, data.batch)
+                out = model(data.x, data.edge_index,
+                            data.edge_attr, data.batch)
             else:
                 out = model(data.x, data.edge_index, data.batch)
 
             loss += criterion(out, data.y) / len(loader)
             acc += accuracy(out.argmax(dim=1), data.y) / len(loader)
 
-    print(f'Model accuracy: {acc * 100:.3f}')
+    print(f'\nModel accuracy on test dataset: {acc * 100:.3f}\n')
+
+    return acc
 
 
-def visualize_results(train_list, val_list, meta_data):
+def visualize_results(train_list, val_list, meta_data, path_to_saving_artifacts):
     """Visualize the loss and valid for train and valid datasets"""
+
     df_1 = pd.DataFrame({'value': train_list})
     df_1['split'] = 'train'
     df_2 = pd.DataFrame({'value': val_list})
@@ -177,28 +235,56 @@ def visualize_results(train_list, val_list, meta_data):
             size=15,
         )
     )
-    fig.show()
+    # fig.show()
+    fig.write_image(os.path.join(path_to_saving_artifacts,
+                    f"images/{meta_data['image_data']}.png"))
 
 
-def run_experiment(model, epochs, edge_feature_compact=True):
+def run_experiment(experiment_name, model, train_loader, val_loader, test_loader, epochs, edge_feature_compact=True):
+    """Run an experiment on given settings"""
+
+    path_to_saving_artifacts = _create_directories(experiment_name)
+    seed_everything()  # apply seeding
     print('Model architecture:\n', model)
+
     trained_model, \
-    train_acc_list, \
-    train_loss_list, \
-    val_acc_list, \
-    val_loss_list = train(model, train_loader, val_loader ,epochs, edge_feature_compact)
+        train_acc_list, \
+        train_loss_list, \
+        val_acc_list, \
+        val_loss_list = train(model, train_loader,
+                              val_loader, epochs, edge_feature_compact)
+
+    # Testing accuracy for the trained model
+    test_accuracy = test_model(
+        trained_model, test_loader, edge_feature_compact)
+    with open(os.path.join(path_to_saving_artifacts, 'accuracy_on_test_dataset.txt'), 'w') as f:
+        f.write(f'Accuracy on test dataset: {test_accuracy}')
 
     # Visualize the loss
-    visualize_results(train_loss_list, val_loss_list, {'title': 'Train and Validation Loss Distribution',
-                                                       'xaxis_title': 'Epoch',
-                                                       'yaxis_title': 'Loss',
-                                                       'legend': 'Split'})
+    visualize_results(train_loss_list, val_loss_list,
+                      {'title': 'Train and Validation Loss Distribution',
+                       'xaxis_title': 'Epoch',
+                       'yaxis_title': 'Loss',
+                       'legend': 'Split',
+                       'image_data': 'losses'},
+                      path_to_saving_artifacts)
     # Visualize the accuracy
     visualize_results([acc * 100 for acc in train_acc_list],
                       [acc * 100 for acc in val_acc_list],
                       {'title': 'Train and Validation Accuracy Distribution',
                        'xaxis_title': 'Epoch',
                        'yaxis_title': 'Accuracy',
-                       'legend': 'Split'})
+                       'legend': 'Split',
+                       'image_data': 'train_val_accuracy'},
+                      path_to_saving_artifacts)
 
-    return trained_model
+    # Saving the trained model
+    print('\nSaving the trained model...\n')
+    # saving entire model
+    torch.save(trained_model, os.path.join(
+        path_to_saving_artifacts, 'models/entire_model.pt'))
+    # saving the model for inference
+    torch.save(trained_model.state_dict(), os.path.join(
+        path_to_saving_artifacts, 'models/inference_model.pt'))
+
+    return trained_model, test_accuracy
